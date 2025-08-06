220a07f52d90c0c74a5075ee7424dbd50a5ce701
shadowdrunk <3144273527@qq.com>
2025-08-07 04:38:27 +0800

    Site updated: 2025-08-07 04:38:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/04/这是一篇新的博文/","2025/08/04/这是一篇新新的文章/","2025/08/04/hello-world/"];function toRandomPost(){
+var posts=["2025/08/04/这是一篇新的博文/","2025/08/04/这是一篇新新的文章/","2025/08/07/李超线段树模板-class/","2025/08/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
